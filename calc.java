bc39d4c5f316efab117d10cd8604b8e55f532ea0
Nakita Rahma <nakitafirmansyah12@gmail.com>
2023-09-07 16:34:05 +0700

    first commit

---
+++ calc.java
@@ -0,0 +1,19 @@
+public class calc {
+    public static void main (String args[]){
+        int x = 10;
+        int y = 5;
+        System.out.print("Angka A: ");
+        System.out.println(x);
+        System.out.print("Angka B: ");
+        System.out.println(y);
+        System.out.print("Hasil Penjumlahan: ");
+        System.out.println(x+y);
+        System.out.print("Hasil Pengurangan: ");
+        System.out.println(x-y);
+        System.out.print("Hasil Perkalian: ");
+        System.out.println(x*y);
+        System.out.print("Hasil Pembagian: ");
+        System.out.println(x/y);
+        
+    }
+}
